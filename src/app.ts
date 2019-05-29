2c61228af10db32ee7f92860df32550d5bf583fb
fcaptuayo <captuayonovoafredy@gmail.com>
2019-05-28 22:48:55 -0500

    Add init execute commant java

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,9 @@ var app = express();
 import {exec, cd} from "shelljs";
 
 app.get('/', function (req, res) {
-
-    return new Promise((resolve, reject) => {
-        exec('java -version', code => {
-            console.log('Exit code:', code);
-            resolve(code);
-        });
+    exec('java -version', code => {
+        console.log('Exit code:', code);
     });
-
     res.send('Hello World!');
 });
 
