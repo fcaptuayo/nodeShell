2cc61c5aa0129b2584cb79e9ca0b07d2a31e575f
irtaimal <ir.taimal10@uniandes.edu.co>
2019-05-30 09:51:17 -0500

    add RIP jar

--- AppStart.ts
+++ AppStart.ts
@@ -8,6 +8,7 @@ export class AppStart {
     private _utilService;
     private _folderService;
     private APK_NAME = "com.evancharlton.mileage_3110-aligned-debugSigned.apk";
+    private APK_FOLDER = "apks";
 
     constructor() {
         this._utilService = new UtilsService();
@@ -15,20 +16,37 @@ export class AppStart {
     }
 
     public async runAllTests() {
-        const self = this;
-        const apksFolder = Path.join(__dirname, '..', 'apks');
-        console.log(apksFolder);
-        const mutationFolders = await this._folderService.findFilesOnFolder(apksFolder, "apk");
+        if (this.validateEnv()) {
+            const self = this;
+            const apksFolder = Path.join(__dirname, '..', this.APK_FOLDER);
+            const mutationFolders = await this._folderService.findFilesOnFolder(apksFolder, "apk");
 
-        for (let index = 0; index < mutationFolders.length; index++) {
-            const item = mutationFolders[index];
-            await self.runSingleTest(item);
+            for (let index = 0; index < mutationFolders.length; index++) {
+                const item = mutationFolders[index];
+                await self.runSingleTest(item);
+            }
         }
     }
 
     public async runSingleTest(mutationFolder) {
         const mutationFilePath = Path.join(mutationFolder, this.APK_NAME);
-        console.log("running test", mutationFilePath);
+        console.log("java -jar RIP.jar rip_config.json", mutationFilePath);
+    }
+
+
+    public async validateEnv() {
+        if (!process.env.AAPT_LOCATION) {
+            console.log("┌───────────────┬───────────────┬───────────────────────────────────────┐");
+            console.log("│   Test is not running !!!                                             │");
+            console.log("│   Before to start the test you need configure the next environment    │");
+            console.log("│   vars must be configured:                                            │");
+            console.log("│                                                                       │");
+            console.log("│   AAPT_LOCATION={Android_SDK_LOCATION}/build-tools/28.0.3/aapt.exe    │");
+            console.log("└───────────────┴───────────────┴───────────────────────────────────────┘");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
 
--- MutationTest.Util/FolderService.ts
+++ MutationTest.Util/FolderService.ts
@@ -4,8 +4,12 @@ export class FolderService {
     public async findFilesOnFolder(folderPath, filter) {
         const self = this;
         if (!fs.existsSync(folderPath)) {
-            console.log("no dir ", folderPath);
-            return;
+            console.log("┌───────────────┬───────────────┬──────────────────────────────────┐");
+            console.log(`│   The folder ${folderPath}            │`);
+            console.log("│   does not exists                                                │");
+            console.log("│   You must ceate folder and include all mutation apks            │");
+            console.log("└───────────────┴───────────────┴──────────────────────────────────┘");
+            return [];
         }
         return fs.readdirSync(folderPath);
     }
